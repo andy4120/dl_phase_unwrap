546fe25eca4fbfb93ec38844432639270fa5b096
Jiwon Choi <cjwon531@gmail.com>
2023-12-25 11:23:23 -0700

    quantitative calculation metrics (MSE, PSNR)

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 import os
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-def img_loader(data_folder: str, input_filename: str, output_filename: str):
+def img_loader(data_folder: str, input_filename: str, output_filename: str) -> tuple(np.ndarray, np.ndarray):
+    '''
+    Read images from dataset folder, split them into X (input) and Y (output) data
+    '''
     subfolders = sorted([f.path for f in os.scandir(data_folder) if f.is_dir()]) # List all subfolders in the data folder
 
     X, Y = [], []
@@ -28,7 +33,7 @@ def img_loader(data_folder: str, input_filename: str, output_filename: str):
 
     return X, Y
 
-def img_read_crop(img_filepath: str, x: int, y: int):
+def img_read_crop(img_filepath: str, x: int, y: int) -> np.ndarray:
     '''
     Read single image for model prediction input, crop into 512 by 512 to match with tensor size
     x,y coordinate is the bound starting coordinate
@@ -40,3 +45,28 @@ def img_read_crop(img_filepath: str, x: int, y: int):
     input_for_prediction = input_image.reshape(1, 512, 512, 1) # reshape to match with tensor size
 
     return input_for_prediction
+
+def mse(img1: np.ndarray, img2: np.ndarray):
+    '''
+    Read 2 images (GT, Pred) -> returns MSE value
+    '''
+    mse = np.mean(np.subtract(img1, img2) ** 2)
+    return mse
+
+def psnr(img1: np.ndarray, img2: np.ndarray):
+    '''
+    Read 2 images (GT, Pred) -> returns PSNR value
+    '''
+    # reference: https://dsp.stackexchange.com/a/50704
+    mse = np.mean((img1 - img2) ** 2)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+
+def calculation_metrics(img1, img2) -> None:
+    '''
+    Fcn call MSE, PSNR quantitative metrics, prints out values
+    '''
+    print('MSE:', mse(img1, img2))
+    print('PSNR:', psnr(img1, img2))
